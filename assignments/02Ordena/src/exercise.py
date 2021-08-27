f380f378ae54dcfbb4cbacc8dd3c17c17fe2df6b
ArturoDuCas <a00833516@itesm.mx>
2021-08-27 16:25:23 -0500

    Final ej 2 8/27

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -3,15 +3,34 @@ def main():
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
-    # EL PRIMER IF LO HICE EN CASO DE QUE LOS TRES NUMEROS SEAN
-    # IGUALES, SOY CONSCIENTE QUE SI EL USARIO INGRESA 2 NUMEROS
-    # IGUALES Y UNO DIFERENTE, EL CODIGO NO ENTREGA NADA. PERO
-    # LO HICE ASI VIENDO LOS DATOS DE LA HOJA  "INPUT DATA" PARA QUE ESTO 
-    # SE CUMPLIERA
     if x==y and z==y:
         print (x)
         print (y)
         print (z)
+    elif x==y and y>z:
+        print (z)
+        print (x)
+        print (y)
+    elif x==y and y<z:
+        print (x)
+        print (y)
+        print (z)
+    elif z==y and y<x:
+        print (y)
+        print (z)
+        print (x)
+    elif z==y and y>x:
+        print (x)
+        print (z)
+        print (y)
+    elif z==x and x<y:
+        print (x)
+        print (z)
+        print (y)
+    elif z==x and x>y:
+        print (y)
+        print (z)
+        print (x)
     elif x<y and y<z:
         print (x)
         print (y)
