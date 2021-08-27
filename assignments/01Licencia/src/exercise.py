bc932c93b7d454881fcb65677040fc72dfeb6b63
ArturoDuCas <a00833516@itesm.mx>
2021-08-27 10:38:43 -0500

    Final ej 1 8/27

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,7 +1,20 @@
 
 def main():
-    pass
-
+    edad = int(input ("Ingresa tu edad: "))
+    if edad >= 18 :
+        iden= (input("¿Tienes identificación oficial? (s/n): "))
+        if  iden=="s":
+            print ("Trámite de licencia concedido")
+        elif iden=="n" :
+            print ("No cumples requisitos")
+        else :
+            print ("Respuesta incorrecta")
+    elif (edad < 18 and edad>=0):
+        print ("No cumples requisitos")
+    else :
+        print ("Respuesta incorrecta")
+    
+    
 
 if __name__ == '__main__':
     main()
